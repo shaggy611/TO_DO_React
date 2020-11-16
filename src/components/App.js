c7fe4a8199010d5be9657968054ae42b97ba7e6b
shaggy611 <sevczik11@gmail.com>
2020-11-16 13:50:03 +0200

    Create main structure of project

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import './normalize.css';
+import AppHeader from'./header'
+
+function App() {
+  return (
+    <div className="App">
+      <AppHeader />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header/index.js
@@ -0,0 +1,2 @@
+import AppHeader from './header';
+export default AppHeader
